cbe61bd68dc90f5a5580cc59ef0709cfa82928bb
Areeb Ali <areeb.algo@outlook.com>
2022-06-22 23:57:45 +0500

    the first react component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Heyy wasssupppp Let's start</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
